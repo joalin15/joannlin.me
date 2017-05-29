b00269386721d9dd524f9ad2a2ba586be5a4c5a0
Joann Lin <joalin15@gmail.com>
2017-05-29 14:38:24 +0800

    updated about

--- js/index.js
+++ js/index.js
@@ -50,31 +50,3 @@
     $(function() {
       $("#connect").load("../html/connect.html");
     });
-
-/*
- * Replace all SVG images with inline SVG
- */
-   $(document).ready(function() {
-     $('img[src$=".svg"]').each(function() {
-         var $img = jQuery(this);
-         var imgURL = $img.attr('src');
-         var attributes = $img.prop("attributes");
-
-         $.get(imgURL, function(data) {
-             // Get the SVG tag, ignore the rest
-             var $svg = jQuery(data).find('svg');
-
-             // Remove any invalid XML tags
-             $svg = $svg.removeAttr('xmlns:a');
-
-             // Loop through IMG attributes and apply on SVG
-             $.each(attributes, function() {
-                console.log("found svg")
-                 $svg.attr(this.name, this.value);
-             });
-
-             // Replace IMG with SVG
-             $img.replaceWith($svg);
-         }, 'xml');
-     });
-    });
