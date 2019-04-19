09c2269571615e287d6b8a49a99aba0bcadda139
joalin15 <joalin15@gmail.com>
2019-04-18 21:47:38 -0700

    fix nav menu on resize

--- datadraft/includes/templates/navigation.php
+++ datadraft/includes/templates/navigation.php
@@ -3,10 +3,15 @@
       <span class="website-title"><a href="index.php">Data Draft</a></span>
     </div>
     <div id="website-nav">
-      <ul class="list">
+      <ul class="list horizontal">
         <li class="list-item"><a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="contact.php">Contact Us</a></li>
         <li class="list-item"><a class="nav-link <?php if ($CURRENT_PAGE == "Team") {?>active<?php }?>" href="team.php">Who We Are</a></li>
         <li class="list-item"><a class="nav-link <?php if ($CURRENT_PAGE == "Home") {?>active<?php }?>" href="index.php">Home</a></li>
+      </ul>
+      <ul class="list vertical">
+        <li class="list-item"><a class="nav-link <?php if ($CURRENT_PAGE == "Home") {?>active<?php }?>" href="index.php">Home</a></li>
+        <li class="list-item"><a class="nav-link <?php if ($CURRENT_PAGE == "Team") {?>active<?php }?>" href="team.php">Who We Are</a></li>
+        <li class="list-item"><a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="contact.php">Contact Us</a></li>
       </ul>
     </div>
 </div>
